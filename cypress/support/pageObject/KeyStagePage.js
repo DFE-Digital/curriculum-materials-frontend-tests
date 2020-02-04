55d48efe7dce513b3a335e5ec75736fa630e3779
Sushant Kumar <Sushant.kumar@QUALITEST.LOCAL>
2020-02-04 11:51:20 +0000

    test code refactoring done

---
+++ cypress/support/pageObject/KeyStagePage.js
@@ -0,0 +1,28 @@
+class KeyStagePage
+{
+
+getKeyStagePageHeader()
+{
+    return cy.get('.govuk-heading-l')
+}
+
+getKeyStagePageSubHeader()
+{
+    return cy.get('.govuk-grid-column-two-thirds > p')
+}
+
+getKeyStageFieldSetHeader()
+{
+    return cy.get('.govuk-fieldset__heading')
+    
+} 
+getKeyStageRadioButton()
+{
+    return cy.get('#year-7')
+}
+getKeyStageContinueButton()
+{
+    return cy.get('.govuk-button')
+}         
+}
+export default KeyStagePage;
--- cypress/support/pageObject/Year7GeographyPage.js
+++ cypress/support/pageObject/Year7GeographyPage.js
@@ -18,5 +18,7 @@ getLessonHeader()
 }
 //article.card >div.card-header > div.card-header-title > a >h3 : list of all lesson header
 //div.card-header-title > span.govuk-caption-m : number of lesson
-//article.card >div.card-body >ul >li :
+//article.card >div.card-body >ul >li :  
+//article.card:nth-child(1) div.card-body > ul:nth-child(2) >li : to get the lesson name
+//article.card:nth-child(5) div.card-header div.card-header-title > span.govuk-caption-m:nth-child(2) : lesson count
 export default Year7GeographyPage;
